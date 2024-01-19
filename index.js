5ca40e52fc32bf72e084bb4bb3218ca0e37318b4
Chris Stevenson <chrislstevenson@gmail.com>
2024-01-19 16:56:08 -0500

    Added required elements

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// TODO: Include packages needed for this application
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+// below is a "local" require not from npm
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [];
